20cef0a89a7249a2f44bad7ead81867cadfe5819
Garrett Pauley <garrettpauley18@gmail.com>
2023-11-27 21:36:00 -0500

    update

--- 83_remove_duplicates_from_sorted_LL.py
+++ 83_remove_duplicates_from_sorted_LL.py
@@ -26,43 +26,9 @@ class ListNode:
         self.next = next
 
 
-def solution(head: ListNode): 
-    vals = []
-    unique_nodes = []
-    while head != None: 
-        if head.val not in vals: 
-            vals.append(head.val)
-            unique_nodes.append(head)
-        head = head.next
-    print ([x.val for x in unique_nodes])
-    unique_nodes.sort(key= lambda x: x.val, reverse=True)
-    newHead = unique_nodes.pop()
-    while unique_nodes: 
-        newHead.next = unique_nodes.pop()
-        newHead = newHead.next
-    return head
-
-def solution2(head: ListNode): 
-    unique_vals = []
-    new = ListNode(head.val)
-    unique_vals.append(head.val)
-   
-    while head:
-        if head.val not in unique_vals: 
-            unique_vals.append(head.val)
-            new.next = head
-            new = new.next
-        head = head.next
-        continue
-    return new
 
 
-def printTest(node: ListNode): 
-    while node: 
-        print(node.val)
-        node = node.next
-
-def solution3(head: ListNode): 
+def solution1(head: ListNode): 
     curr = head
     while curr and curr.next: 
         if curr.val == curr.next.val: 
@@ -71,11 +37,25 @@ def solution3(head: ListNode):
             curr = curr.next
     return head
 
+def solution2(head: ListNode): 
+
+    prev = head 
+    current = head.next
+
+    while current: 
+        # If the current node's value is equal to the previous node - delete the current node
+        if current.val == prev.val: 
+            prev.next = current.next
+            current = current.next
+        # Move the pointers forward
+        else: 
+            prev = current
+            current = current.next
+    return head
+
 
 t = ListNode(1, ListNode(2, ListNode(3, ListNode(3)))) 
 # print(solution(t))
 s = solution3(t)
-printTest(s)
-
 
 
